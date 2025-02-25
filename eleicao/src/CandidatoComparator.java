0a613c714d8507c01d68df91dfb3e6af6c9be2c0
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-02-25 09:38:51 -0300

    ajuste comparação Idade

--- eleicao/src/CandidatoComparator.java
+++ eleicao/src/CandidatoComparator.java
@@ -3,7 +3,9 @@ class CandidatoComparator implements java.util.Comparator<Candidato> {
     public int compare(Candidato a, Candidato b){
         int dif = b.getNumVotos() - a.getNumVotos();
         if(dif == 0){
-            dif = b.getIdade() - a.getIdade();
+            if(b.getNasc().isBefore(a.getNasc())) dif = 1;
+            else if(b.getNasc().isAfter(a.getNasc())) dif = -1;
+            else dif = 0;
         }
        return dif;
     }
