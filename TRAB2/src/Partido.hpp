78c16c470c84be7a31238e9b328e5c0a4d847e13
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-03-10 00:36:29 -0300

    ajustes candidato e partido

--- TRAB2/src/Partido.hpp
+++ TRAB2/src/Partido.hpp
@@ -3,18 +3,19 @@
 
 #include <iostream>
 #include <string>
+#include <list>
+#include <bits/stdc++.h>
+
+#include "Candidato.hpp"
 
 using namespace std;
-
-//#include "Candidato.hpp" //MARCELA:isso vai ruim pq um não pode conter o outro
 
 class Partido
 {
     string nome, sigla;
     int numPartido, qtdVotosLegenda;
-    //MARCELA: ver como vamos fazer isso em c++
-    // fazer uma estrutura lista que vai ter o candidato dentro da célula???????
-    // LinkedList<Candidato> candidatos;
+    list<Candidato> candidatos;
+    
 public:
     Partido(const string &nome, const string &sigla, const int &numPartido);
     void incrementaVotosLegenda(const int &votos);
@@ -22,14 +23,13 @@ public:
     const string &getNome() const;
     const int &getNumPartido() const;
     const int &getQtdVotosLegenda() const;
-    const int &getQtdCandidatos() const;
-	const int &qtdVotosNominais() const;
-    const int &getQtdVotosTotais() const;
-    const int &getQtdVotosNominais() const;
-    const int &getQtdEleitosNoPartido() const;
-    //void adicionaCandidato(Candidato c);
-    //LinkedList<Candidato> getCandidatos();
-    //LinkedList<Candidato> getCandidatosOrdenados();
+    int getQtdCandidatos() const;
+    int getQtdVotosTotais() const;
+    int getQtdVotosNominais() const;
+    int getQtdEleitosNoPartido() const;
+    void adicionaCandidato(Candidato &c);
+    list<Candidato> getCandidatos();
+    list<Candidato> getCandidatosOrdenados();
 
 };
 
